c07dba66226bd5fbf8f075dde286f02170dee16d
woojh3690 <woojh3690@gmail.com>
2022-08-05 18:18:39 +0900

    2차원 배열 버퍼를 위한 table_queue.py 작성 완료

--- table_queue.py
+++ table_queue.py
@@ -1,7 +1,14 @@
 class TableQueue:
     def __init__(self, row_size, col_size):
-        self.table = [['None'] * col_size] * row_size
+        self.queue = []
+        for _ in range(col_size):
+            self.queue.append(['None'] * row_size)
+        self.row_size = row_size
+        self.col_size = col_size
     
+    # 버퍼에 데이터를 저장한다. 
+    # 버퍼에 저장되는 데이터 개수는 row_size 넘지 않는다.
     def push(self, idx, value):
-        print("table : ", self.table)
-        # TODO
+        sel_queue = self.queue[idx]
+        sel_queue.append(value)
+        sel_queue.pop(0)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from table_queue import TableQueue
+import numpy as np
 
 if __name__ == '__main__':
     tableQueue = TableQueue(24, 6)
     tableQueue.push(2, 3)
+
+    np_data = np.char.isnumeric(tableQueue.queue)
+    print(np_data)
+    print(np_data.sum())
