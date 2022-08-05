6a4125640e00e8b814fbc6826c1c41f06f9d64a8
woojh3690 <woojh3690@gmail.com>
2022-08-05 13:42:54 +0900

    받은 메시지 json 파싱 코드 추가

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from kafka_manager import KafkaManager
+from json import loads, dumps
+
 
 # 메인 클래스
 class main(KafkaManager):
+
     def __init__(self):
         super().__init__("192.168.0.217", "9092")
+        self.buffer = []
 
     def start(self):
         print("main_start")
         for message in self.consumer:
-            print("Topic: {}, Partition: {}, Offset: {}, Key: {}, Value: {}"
-                .format(message.topic, message.partition, message.offset, message.key, message.value)
-            )
+            try:
+                jsonObj = loads(message.value)
+            except: pass
+
+            print("받은 메시지 : ", jsonObj)
+            print("받은 시간 : ", jsonObj['msg_data']['timestamp'])
+            print("json : ", jsonObj['msg_data']['features'])
 
 if __name__ == '__main__':
     main = main()
