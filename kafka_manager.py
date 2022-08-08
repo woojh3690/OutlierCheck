e98c22fa21cdab00a5c62bf23a1029351c1d38c3
woojh3690 <woojh3690@gmail.com>
2022-08-08 13:19:25 +0900

    kafka utf-8 en, de serializer 추가

--- kafka_manager.py
+++ kafka_manager.py
@@ -1,4 +1,5 @@
 from kafka import KafkaConsumer, KafkaProducer
+from json import loads, dumps
 
 class KafkaManager:
 
@@ -10,11 +11,18 @@ class KafkaManager:
             bootstrap_servers=[server_url],
             auto_offset_reset='latest',
             enable_auto_commit=True,
-            group_id='outlier_checker'
+            group_id='outlier_checker',
+            value_deserializer=lambda x: x.decode('utf-8'),
+            consumer_timeout_ms=5000
         )
         
         self.producer = KafkaProducer(
             acks=0, 
-            bootstrap_servers = [server_url]
+            bootstrap_servers = [server_url],
+            value_serializer=lambda x: x.encode('utf-8')
         )
+    
+    def kafka_close(self):
+        self.consumer.close()
+        self.producer.close()
 
