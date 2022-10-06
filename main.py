1396086f1727835906fd8068fffec2035640c6bb
woojh3690 <woojh3690@gmail.com>
2022-10-06 11:44:44 +0900

    리펙토링: main.py 상위 클래스 생성자 호출 순서 수정

--- main.py
+++ main.py
@@ -14,27 +14,23 @@ class main(Thread, KafkaManager):
     def __init__(self, args):
         if (len(args) != 5): print('유효한 인자가 없습니다.')
 
-        KafkaManager.__init__(self, args[1], args[2])
         Thread.__init__(self)
+        KafkaManager.__init__(self, args[1], args[2])
 
         # 모델 메타데이터 초기화
         for modelMeta in self.readModelMeta():
             if modelMeta['model_code'] == args[4]:
                 self.modelMeta = modelMeta
 
-        self.WINDOW_SIZE = self.modelMeta['window_size']   # 윈도우 사이즈
-        self.buffer = []                                        # 데이터 버퍼
-        self.work_queue = Queue()                               # 작업 명령 큐
-        self.workers = []                                       # 모델 실행 스레드 리스트
-        self.is_end = False                                     # 종료 이벤트
+        self.WINDOW_SIZE = self.modelMeta['window_size']    # 윈도우 사이즈
+        self.buffer = []                                    # 데이터 버퍼
+        self.work_queue = Queue()                           # Thread-safe 작업 명령 큐
+        self.workers = []                                   # 모델 실행 스레드 리스트
+        self.is_end = False                                 # 종료 이벤트
 
         # TF 모델 초기화
         for id in range(int(args[3])):
-            worker = LstmWorker(
-                id,
-                self.work_queue,
-                modelMeta
-            )
+            worker = LstmWorker(id, self.work_queue, modelMeta)
             worker.start()
             self.workers.append(worker)
 
